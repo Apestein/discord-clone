44c2b8b5ad1c709c99d42d6ee462ac9905f0b7a9
annony <ltn2057@protonmail.com>
2022-12-19 21:36:07 -0500

    styling component channelX + responsive design

--- src/components/ChannelX.tsx
+++ src/components/ChannelX.tsx
@@ -1,19 +1,34 @@
 import React from "react"
+import "../styles/ChannelX.css"
 import { ReactComponent as AtIcon } from "../assets/@.svg"
 import { ReactComponent as ThreadIcon } from "../assets/thread.svg"
 import { ReactComponent as BellIcon } from "../assets/bell.svg"
 import { ReactComponent as PinIcon } from "../assets/pin.svg"
 import { ReactComponent as MembersIcon } from "../assets/members.svg"
+import { ReactComponent as InboxIcon } from "../assets/inbox.svg"
+import { ReactComponent as HelpIcon } from "../assets/help.svg"
 
 export default function ChannelX() {
   return (
     <>
-      <div className="h-[48px] border-b-2 border-[#00000050] flex">
-        <AtIcon />
+      <div className="app_header">
+        <div className="app_wrapper1">
+          <AtIcon className="app_at-icon" />
+          <p className="">odin-offtopic</p>
+          <div className="app_divider"></div>
+          <p className="app_promo">
+            No self promotion, asking for help should go in the proper help
+            channel. off-topic is for relaxing, not asking help questions. Not a
+            meme channel.
+          </p>
+        </div>
         <ThreadIcon />
         <BellIcon />
         <PinIcon />
         <MembersIcon />
+        <input type="search" placeholder="Search" />
+        <InboxIcon />
+        <HelpIcon />
       </div>
     </>
   )
